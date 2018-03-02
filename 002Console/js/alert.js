cb8d92cc16cd1f4cf6d26db6c279aa542ed24e2a
Prymetheus <prymetheus@github.com>
2018-03-03 00:20:11 +0800

    外链js文件

---
+++ 002Console/js/alert.js
@@ -0,0 +1,12 @@
+// alert("欢迎天下第一程序员")
+// alert("你怎么打开了这个网页")
+// /*alert("我知道你有点好奇")
+// alert("再等等")*/
+// alert("也许有奇迹")
+alert("这是最后一句吗")
+alert("I love you 拜拜")
+function test() {
+    alert("欢迎天下第一程序员")
+    alert("你怎么打开了这个网页")
+}
+test();
